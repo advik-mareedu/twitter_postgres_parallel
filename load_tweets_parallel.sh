9a86cb00e98bc24d27ca0e3247ef9d81f0896efb
Advik Mareedu <amareedu@g.hmc.edu>
2026-04-12 22:52:12 -0700

    updated

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -12,13 +12,10 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
-time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py --inputs="$file" --db postgresql://postgres:pass@localhost:1200 &
+time echo "$files" | parallel python3 load_tweets.py --inputs={} --db postgresql://postgres:pass@localhost:1200
 
-done
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel python3 load_tweets_batch.py --inputs={} --db postgresql://postgres:pass@localhost:1300
