d8777418da452327c6285a6ae83ff28543ad183a
Advik Mareedu <amareedu@g.hmc.edu>
2026-04-12 18:17:34 -0700

    part 1

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -7,6 +7,19 @@ echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
     echo
+    unzip -p "$file" | \
+    jq -c 'walk(
+      if type=="string" then
+        gsub("\\\\"; "\\\\\\\\") |  # escape backslashes
+        gsub("\r"; "")      |        # remove carriage returns
+        gsub("\n"; "\\n")  |        # escape newlines
+        gsub("\""; "\\\"")           # escape quotes
+      else
+        .
+      end
+    )' | \
+    psql postgresql://postgres:pass@localhost:1100 \
+      -c "\COPY tweets_jsonb (data) FROM STDIN"
     # copy your solution to the twitter_postgres assignment here
 done
 
@@ -16,6 +29,8 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
+    python3 load_tweets.py --inputs="$file" --db postgresql://postgres:pass@localhost:1200
+
 done
 
 echo '================================================================================'
