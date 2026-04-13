d67bca0403bc5122db272ccc4bc4967416301abb
Advik Mareedu <amareedu@g.hmc.edu>
2026-04-12 22:17:14 -0700

    updated

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,12 +6,18 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time for file in $files; do
+    echo
+    # copy your solution to the twitter_postgres assignment here
+    python3 load_tweets.py --inputs="$file" --db postgresql://postgres:pass@localhost:1200 &
 
+done
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
